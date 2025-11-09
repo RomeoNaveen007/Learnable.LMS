17d5c2b9501d3c5537f5fa7c0b549349bdd1f58d
RomeoNaveen007 <raveenthirannaveen7@gmail.com>
2025-11-09 11:12:31 -0800

    Added BuggyController

---
+++ Learnable.WebApi/Controllers/BaseController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Learnable.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseController : ControllerBase
+    {
+    }
+}
