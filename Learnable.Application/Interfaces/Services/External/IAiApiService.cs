18e4923cd78105e79b3474ef4b78fb09303606a3
Kajaa <kajaatharan1112@gmail.com>
2025-11-21 15:43:36 +0530

    ai_service_working

---
+++ Learnable.Application/Interfaces/Services/External/IAiApiService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learnable.Application.Interfaces.Services.External
+{
+    internal interface IAiApiService
+    {
+    }
+}
