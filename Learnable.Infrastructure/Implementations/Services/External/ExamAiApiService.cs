78b935bcc808d62b1bc9991de12c08ab34e627e4
Kajaa <kajaatharan1112@gmail.com>
2025-11-27 21:57:47 +0530

    ExamAiApiService

---
+++ Learnable.Infrastructure/Implementations/Services/External/ExamAiApiService.cs
@@ -0,0 +1,14 @@
+﻿using Learnable.Application.Interfaces.Services.External;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learnable.Infrastructure.Implementations.Services.External
+{
+    public class ExamAiApiService: IExamAiApiService
+    {
+
+    }
+}
